e9747a7c8df7621e78eefa8c6f24a16cc136b06b
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-04-27 22:06:17 +0200

    First version of console.py.

---
+++ src/tutompy/console.py
@@ -0,0 +1,7 @@
+import click
+from . import __version__
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    click.echo("Hello, world!")
