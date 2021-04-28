1f7a745ceae49e7755b4e6935ea1eaed31ec6092
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-04-28 11:00:50 +0200

    pytest.fixture added to runner.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,9 +1,15 @@
+# tests/test_console.py
 import click.testing
+import pytest
 
-from tutompy import console
+from hypermodern_python import console
 
 
-def test_main_succeeds():
-    runner = click.testing.CliRunner()
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
