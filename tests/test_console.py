3decdbe31dcd0f29479648780451997c5a521bc8
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-04-28 23:12:52 +0200

    pytest-mock added.

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,7 +9,15 @@ from tutompy import console
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
