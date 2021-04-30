d439644bbc1fded58556c7dd0dfb6e7a5d9891d0
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-04-30 11:55:56 +0200

    Add test for mock call_args.

--- tests/test_console.py
+++ tests/test_console.py
@@ -25,3 +25,8 @@ def test_main_succeeds(runner, mock_requests_get):
 def test_main_invokes_requests_get(runner, mock_requests_get):
     runner.invoke(console.main)
     assert mock_requests_get.called
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
