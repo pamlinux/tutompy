c04f5d655904746e2ad223f2e4bbf5ebf20ce0be
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-01 23:16:53 +0200

    flake8 configured with flake8-black warnings.

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 # noxfile.py
 import nox
 
+nox.options.sessions = "lint", "tests"
+
 locations = "src", "tests", "noxfile.py"
 
 
@@ -14,7 +16,7 @@ def tests(session):
 @nox.session(python=["3.8", "3.9"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
 
 
