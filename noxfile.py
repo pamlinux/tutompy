530ea983b9f2afa27dd2f9388a4d89813a20722d
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-02 17:12:21 +0200

    Added safety. forgotten file conftets.py added too.

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
 # noxfile.py
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 locations = "src", "tests", "noxfile.py"
 
@@ -31,3 +33,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
