b1d9d7509a4b85c62117f1d42e14d5122c5cb5a6
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-03 12:24:12 +0200

    Data validation using Desert and Marshmallow.

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,3 +1,8 @@
+from unittest.mock import Mock
+
+import click
+import pytest
+
 from tutompy import wikipedia
 
 
@@ -5,3 +10,14 @@ def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="fr")
     args, _ = mock_requests_get.call_args
     assert "fr.wikipedia.org" in args[0]
+
+
+def test_random_page_returns_page(mock_requests_get):
+    page = wikipedia.random_page()
+    assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
